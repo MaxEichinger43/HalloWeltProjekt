004923d4862bbeaf3be0082942ceb3a74cdf8403
MaxEichinger43 <126464027+MaxEichinger43@users.noreply.github.com>
2023-02-28 10:21:25 +0100

    Add files via upload

---
+++ Test repo/scripts/HalloWelt.cs
@@ -0,0 +1,12 @@
+using System;
+
+    class HalloWelt
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt!");
+
+        
+            Console.ReadKey();
+        }
+    }
